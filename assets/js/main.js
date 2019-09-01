27a9d70e4e590ea1e4787676377e959f9c6287c5
metamezzo <metamezzo@users.noreply.github.com>
2019-09-01 15:43:26 +0100

    Update console log message

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,6 @@ const scss = require('../scss/main.scss');
   // Say hi to console visitors
   console.log("%c Hello from PICODELIQ ", "background-color: #3498db; color: #fff; font-size: 1.168em; padding-top: 5px; padding-bottom: 5px");
   console.log("%c Do you have a project or digital product that could use extra fresh eyes and hands so that on launch it evokes awesomeness straightaway? Then we ❤ you already. Say \"Hiya!\" at hello@picodeliq.com ", "background-color: #3498db; color: #fff; padding-top: 5px; padding-bottom: 5px");
-  console.log("%c If you're just curious…we are using a custom Divi child theme for this iteration of our website 😊 ", "background-color: #3498db; color: #fff; padding-top: 5px; padding-bottom: 5px");
+  console.log("%c If you're just curious… we are using a custom Divi child theme for this iteration of our website 😊 ", "background-color: #3498db; color: #fff; padding-top: 5px; padding-bottom: 5px");
 
 })(jQuery);
